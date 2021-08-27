7e9005d43ea134f8da49b6843c34862fc4ce1a99
popjirapat <jirapat486@gmail.com>
2021-08-27 11:43:33 +0700

    แบบประเมินข้อ3,ใส่ alert ตรงปุ่ม back หน้าแบบประเมินข้อ1-3

--- www/js/evaluate.js
+++ www/js/evaluate.js
@@ -33,6 +33,17 @@ $(function () {
       showModal("modal-evaluate-detail");
     }, 500);
   }
+  // ปุ่ม ยืนยัน modal-evaluate-alert
+  $("#modal-evaluate-alert .submit_alret").on("click", function () {
+    changePage("evaluate_detail_page", function () {});
+    $(".modal").hide();
+    $("body").removeClass("modal-open");
+  });
+  // ปุ่ม ยกเลิก modal-evaluate-alert
+  $("#modal-evaluate-alert .cancel_alret").on("click", function () {
+    $(".modal").hide();
+    $("body").removeClass("modal-open");
+  });
   /* ----------------------------------------------------------------------------- start : evaluate_page ----------------------------------------------------------------------------- */
   $("#evaluate_page .header .back_header_btn").on("click", function () {
     $(".menu_home_page").click();
@@ -65,8 +76,10 @@ $(function () {
         loading.hide();
         changePage("evaluate_page_1", function () {
           $("#DTX").prop("disabled", true);
-          $("#evaluate_page_1 .step-footer").hide();
           $("#DTX").val("75");
+          $("#evaluate_page_1 .evaluate_page_status ").show();
+          $("#evaluate_page_1 .footer").show();
+          $("#evaluate_page_1 .step-footer").hide();
         });
       }, 500);
     });
@@ -74,7 +87,7 @@ $(function () {
   $("#evaluate_page_1 .evaluate_page_header .back_header_btn").on(
     "click",
     function () {
-      changePage("evaluate_detail_page", function () {});
+      showModal("modal-evaluate-alert");
     }
   );
   // ปุ่ม เริ่มประเมินใหม่
@@ -142,6 +155,10 @@ $(function () {
         changePage("evaluate_page_2", function () {
           $("#blood_pressure_up").prop("disabled", true);
           $("#blood_pressure_down").prop("disabled", true);
+          $("#blood_pressure_up").val("75");
+          $("#blood_pressure_down").val("75");
+          $("#evaluate_page_2 .evaluate_page_status ").show();
+          $("#evaluate_page_2 .footer").show();
           $("#evaluate_page_2 .step-footer").hide();
         });
       }, 500);
@@ -150,7 +167,7 @@ $(function () {
   $("#evaluate_page_2 .evaluate_page_header .back_header_btn").on(
     "click",
     function () {
-      changePage("evaluate_detail_page", function () {});
+      showModal("modal-evaluate-alert");
     }
   );
   // ปุ่ม เริ่มประเมินใหม่
@@ -231,6 +248,12 @@ $(function () {
       });
     }
   );
+  // $("#evaluate_detail_page .footer .btn_create_evaluate").on(
+  //   "click",
+  //   function () {
+  //     showModal("modal-evaluate-alert");
+  //   }
+  // );
   /* ----------------------------------------------------------------------------- end : evaluate_page_2 ----------------------------------------------------------------------------- */
 
   /* ----------------------------------------------------------------------------- start : evaluate_page_3 ----------------------------------------------------------------------------- */
@@ -242,12 +265,48 @@ $(function () {
       setTimeout(function () {
         loading.hide();
         changePage("evaluate_page_3", function () {
+          $("#evaluate_page_3 .content .form img").remove();
+          $("#evaluate_page_3 .content .form button").removeClass(
+            "btnSelector"
+          );
           $("#evaluate_page_3 .content .form button").prop("disabled", true);
           $("#CVD1N").addClass("btnSelector");
           $("#CVD1N").addClass("btnDisabled");
           $("#CVD1N").prepend(
             '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
           );
+          $("#CVD2N").addClass("btnSelector");
+          $("#CVD2N").addClass("btnDisabled");
+          $("#CVD2N").prepend(
+            '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+          );
+          $("#CVD3N").addClass("btnSelector");
+          $("#CVD3N").addClass("btnDisabled");
+          $("#CVD3N").prepend(
+            '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+          );
+          $("#CVD4N").addClass("btnSelector");
+          $("#CVD4N").addClass("btnDisabled");
+          $("#CVD4N").prepend(
+            '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+          );
+          $("#CVD5N").addClass("btnSelector");
+          $("#CVD5N").addClass("btnDisabled");
+          $("#CVD5N").prepend(
+            '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+          );
+          $("#CVD6N").addClass("btnSelector");
+          $("#CVD6N").addClass("btnDisabled");
+          $("#CVD6N").prepend(
+            '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+          );
+          $("#CVD7N").addClass("btnSelector");
+          $("#CVD7N").addClass("btnDisabled");
+          $("#CVD7N").prepend(
+            '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+          );
+          $("#evaluate_page_3 .evaluate_page_status ").show();
+          $("#evaluate_page_3 .footer").show();
           $("#evaluate_page_3 .step-footer").hide();
         });
       }, 500);
@@ -256,7 +315,7 @@ $(function () {
   $("#evaluate_page_3 .evaluate_page_header .back_header_btn").on(
     "click",
     function () {
-      changePage("evaluate_detail_page", function () {});
+      showModal("modal-evaluate-alert");
     }
   );
   // ปุ่ม เริ่มประเมินใหม่
@@ -280,28 +339,79 @@ $(function () {
   $("#CVD1Y").on("click", function () {
     toggleBtnAnsEva("CVD1", "CVD1Y");
   });
+  // answer CVD2
+  $("#CVD2N").on("click", function () {
+    toggleBtnAnsEva("CVD2", "CVD2N");
+  });
+  $("#CVD2Y").on("click", function () {
+    toggleBtnAnsEva("CVD2", "CVD2Y");
+  });
+  // answer CVD3
+  $("#CVD3N").on("click", function () {
+    toggleBtnAnsEva("CVD3", "CVD3N");
+  });
+  $("#CVD3Y").on("click", function () {
+    toggleBtnAnsEva("CVD3", "CVD3Y");
+  });
+  // answer CVD4
+  $("#CVD4N").on("click", function () {
+    toggleBtnAnsEva("CVD4", "CVD4N");
+  });
+  $("#CVD4Y").on("click", function () {
+    toggleBtnAnsEva("CVD4", "CVD4Y");
+  });
+  // answer CVD5
+  $("#CVD5N").on("click", function () {
+    toggleBtnAnsEva("CVD5", "CVD5N");
+  });
+  $("#CVD5Y").on("click", function () {
+    toggleBtnAnsEva("CVD5", "CVD5Y");
+  });
+  // answer CVD6
+  $("#CVD6N").on("click", function () {
+    toggleBtnAnsEva("CVD6", "CVD6N");
+  });
+  $("#CVD6Y").on("click", function () {
+    toggleBtnAnsEva("CVD6", "CVD6Y");
+  });
+  // answer CVD7
+  $("#CVD7N").on("click", function () {
+    toggleBtnAnsEva("CVD7", "CVD7N");
+  });
+  $("#CVD7Y").on("click", function () {
+    toggleBtnAnsEva("CVD7", "CVD7Y");
+  });
   // เช็คค่าในแบบประเมินเพื่อเปิดปุ่มบันทึก
-  // $("#evaluate_page_3 .content .form button").on(
-  //   "change paste keyup",
-  //   function () {
-  //     if (
-  //       $("#blood_pressure_up").val().length != 0 &&
-  //       $("#blood_pressure_down").val().length != 0
-  //     ) {
-  //       $("#evaluate_page_2 .step-footer .btn_group .submit").prop(
-  //         "disabled",
-  //         false
-  //       );
-  //       $("#evaluate_page_2 .evaluate_page_status ").show();
-  //     } else {
-  //       $("#evaluate_page_2 .step-footer .btn_group .submit").prop(
-  //         "disabled",
-  //         true
-  //       );
-  //       $("#evaluate_page_2 .evaluate_page_status ").hide();
-  //     }
-  //   }
-  // );
+  $(".answer button").on("click", function () {
+    if (
+      $("#CVD1Y").hasClass("btnSelector") ||
+      $("#CVD1N").hasClass("btnSelector") ||
+      $("#CVD2Y").hasClass("btnSelector") ||
+      $("#CVD2N").hasClass("btnSelector") ||
+      $("#CVD3Y").hasClass("btnSelector") ||
+      $("#CVD3N").hasClass("btnSelector") ||
+      $("#CVD4Y").hasClass("btnSelector") ||
+      $("#CVD4N").hasClass("btnSelector") ||
+      $("#CVD5Y").hasClass("btnSelector") ||
+      $("#CVD5N").hasClass("btnSelector") ||
+      $("#CVD6Y").hasClass("btnSelector") ||
+      $("#CVD6N").hasClass("btnSelector") ||
+      $("#CVD7Y").hasClass("btnSelector") ||
+      $("#CVD7N").hasClass("btnSelector")
+    ) {
+      $("#evaluate_page_3 .step-footer .btn_group .submit").prop(
+        "disabled",
+        false
+      );
+      $("#evaluate_page_3 .evaluate_page_status ").show();
+    } else {
+      $("#evaluate_page_3 .step-footer .btn_group .submit").prop(
+        "disabled",
+        true
+      );
+      $("#evaluate_page_3 .evaluate_page_status ").hide();
+    }
+  });
   // ปุ่ม ยกเลิก
   $("#evaluate_page_3 .step-footer .btn_group .cancel").on(
     "click",
@@ -309,6 +419,41 @@ $(function () {
       $("#evaluate_page_3 .content .form img").remove();
       $("#evaluate_page_3 .content .form button").removeClass("btnSelector");
       $("#evaluate_page_3 .content .form button").prop("disabled", true);
+      $("#CVD1N").addClass("btnSelector");
+      $("#CVD1N").addClass("btnDisabled");
+      $("#CVD1N").prepend(
+        '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+      );
+      $("#CVD2N").addClass("btnSelector");
+      $("#CVD2N").addClass("btnDisabled");
+      $("#CVD2N").prepend(
+        '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+      );
+      $("#CVD3N").addClass("btnSelector");
+      $("#CVD3N").addClass("btnDisabled");
+      $("#CVD3N").prepend(
+        '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+      );
+      $("#CVD4N").addClass("btnSelector");
+      $("#CVD4N").addClass("btnDisabled");
+      $("#CVD4N").prepend(
+        '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+      );
+      $("#CVD5N").addClass("btnSelector");
+      $("#CVD5N").addClass("btnDisabled");
+      $("#CVD5N").prepend(
+        '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+      );
+      $("#CVD6N").addClass("btnSelector");
+      $("#CVD6N").addClass("btnDisabled");
+      $("#CVD6N").prepend(
+        '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+      );
+      $("#CVD7N").addClass("btnSelector");
+      $("#CVD7N").addClass("btnDisabled");
+      $("#CVD7N").prepend(
+        '<img class="btnIconSelector" src="img/check_icon_disabled.png" />'
+      );
       $("#evaluate_page_3 .evaluate_page_status ").show();
       $("#evaluate_page_3 .footer").show();
       $("#evaluate_page_3 .step-footer").hide();
